ce83c3ebc012b7767d0054883ab7bfeaf4572593
taroj1205 <taroj1205@gmail.com>
2025-04-28 17:24:54 +1200

    feat(suspense): add console.log to show its server function

--- apps/suspense/utils/some-func.ts
+++ apps/suspense/utils/some-func.ts
@@ -1,6 +1,8 @@
 
 export const someFunc = async (sleep = 3000) => {
   'use server'
+  console.log("someFunc called");
   await new Promise((resolve) => setTimeout(resolve, sleep));
+  console.log("someFunc resolved");
   return "Hello, world!";
 };
